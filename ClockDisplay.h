bc4340489b5e8bb383d359736bee2943078cb878
Andrew <amelliott@gmail.com>
2013-05-20 22:53:52 -0700

    Fixing blinking state for date mode

--- ClockDisplay.h
+++ ClockDisplay.h
@@ -2,6 +2,7 @@
 class ClockDisplay {
     public:
         virtual void displayTime() = 0;
+        virtual void displayDate() = 0;
         virtual void startBlink() = 0;
         virtual void stopBlink() = 0;
 };
@@ -11,9 +12,12 @@ class ConsoleClockDisplay : public ClockDisplay {
         ConsoleClockDisplay();
         ~ConsoleClockDisplay();
         void displayTime();
+        void displayDate();
         void startBlink();
         void stopBlink();
     private:
+        bool needToBlink();
         bool blinking;
+        char displayString[9];
 };
 
